34e8f3ec3f9cd9635e8c551dfb3a7b16c33a2e4e
Marcin <marcin@radgost.com>
2020-07-01 20:47:04 +0200

    init bundle gem render_json_rails

---
+++ lib/render_json_rails.rb
@@ -0,0 +1,6 @@
+require "render_json_rails/version"
+
+module RenderJsonRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/render_json_rails/version.rb
@@ -0,0 +1,3 @@
+module RenderJsonRails
+  VERSION = "0.1.0"
+end
