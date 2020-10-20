07063a65a270a7c14b08d42b7537d9d5a52d8475
Tomasz Bogus <tomasz.bogus@radgost.com>
2020-10-20 18:21:14 +0200

    Add default options test

--- test/render_json_rails_test.rb
+++ test/render_json_rails_test.rb
@@ -11,25 +11,37 @@ class TestModel1
 
 end
 
+class DefaultFieldsModel
+
+  include RenderJsonRails::Concern
+  render_json_config name: :default_fields_model,
+                     default_fields: [:id, :account_id, :name],
+                     except: [:account_id]
+
+end
+
 class RenderJsonRailsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil RenderJsonRails::VERSION
   end
 
   def test_model1
-    # assert false
-    # m1 = TestModel1.new
-
     out = TestModel1.render_json_options()
-    expected = {
-      except: [:account_id],
-    }
+    expected = { except: [:account_id] }
     assert_equal expected, out, "out: #{out}"
 
-    out = TestModel1.render_json_options(fields: {'model1' => "id,account_id  , name" })
-    expected = {
-      only: [:id, :name],
-    }
+    out = TestModel1.render_json_options(fields: { "model1" => "id,account_id,name" })
+    expected = { only: [:id, :name] }
+    assert_equal expected, out, "out: #{out}"
+  end
+
+  def test_default_fields_model
+    out = DefaultFieldsModel.render_json_options()
+    expected = { only: [:id, :name] }
+    assert_equal expected, out, "out: #{out}"
+
+    out = DefaultFieldsModel.render_json_options(fields: { "default_fields_model" => "id,account_id,kind,user"})
+    expected = { only: [:id, :kind, :user] }
     assert_equal expected, out, "out: #{out}"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 # require 'active_support'
 require 'active_support/all'
 
+require "byebug"
 require "render_json_rails"
 require "minitest/autorun"
